2ed0a85e6079462ae0fffd602a4ad9fff90e5d41
Volodymyr Voronov <malones.vladimir@gmail.com>
2024-07-16 16:20:27 +0200

    Updated store.

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,6 +1,7 @@
 import Papa from "papaparse";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
+import { persist, createJSONStorage } from "zustand/middleware";
 
 import { IParsedCSVFile } from "../types/types";
 
@@ -18,43 +19,50 @@ export interface IAppActions {
   setCurrentStep: (step: number) => void;
 }
 
-export const useAppStore = create(
-  immer<IAppStore & IAppActions>((set, get) => ({
-    rawCSVFile: null,
-    parsedCSVFile: null,
-    fileName: "",
-    currentStep: 0,
+type StoreWithActions = IAppStore & IAppActions;
 
-    setCSVFile: (file: string) => {
-      const parsedNewCSVFiles = Papa.parse(file, {
-        header: true,
-        dynamicTyping: true,
-        skipEmptyLines: true,
-      }) as IParsedCSVFile;
+export const useAppStore = create<StoreWithActions>()(
+  immer(
+    persist(
+      (set, get) => ({
+        rawCSVFile: null,
+        parsedCSVFile: null,
+        fileName: "",
+        currentStep: 0,
 
-      set((state) => {
-        state.rawCSVFile = file;
-        state.parsedCSVFile = parsedNewCSVFiles;
-      });
-    },
+        setCSVFile: (file: string) => {
+          const parsedNewCSVFiles = Papa.parse(file, {
+            header: true,
+            dynamicTyping: true,
+            skipEmptyLines: true,
+          }) as IParsedCSVFile;
 
-    resetCSVFile: () => {
-      set((state) => {
-        state.rawCSVFile = null;
-        state.parsedCSVFile = null;
-      });
-    },
+          set((state) => {
+            state.rawCSVFile = file;
+            state.parsedCSVFile = parsedNewCSVFiles;
+          });
+        },
 
-    setFileName: (file: string) => {
-      set((state) => {
-        state.fileName = file;
-      });
-    },
+        resetCSVFile: () => {
+          set((state) => {
+            state.rawCSVFile = null;
+            state.parsedCSVFile = null;
+          });
+        },
 
-    setCurrentStep: (step: number) => {
-      set((state) => {
-        state.currentStep = step;
-      });
-    },
-  })),
+        setFileName: (file: string) => {
+          set((state) => {
+            state.fileName = file;
+          });
+        },
+
+        setCurrentStep: (step: number) => {
+          set((state) => {
+            state.currentStep = step;
+          });
+        },
+      }),
+      { name: "pp-app", storage: createJSONStorage(() => localStorage) },
+    ),
+  ),
 );
